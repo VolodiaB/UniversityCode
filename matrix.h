3c0d88220e9b37c12b533e7b7a79df0ed0119764
VolodiaB <baymakov1997@mail.ru>
2016-11-27 19:51:04 +0400

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,19 @@
+//
+// Created by volodia on 27.11.16.
+//
+
+#ifndef SERVEROK_MATRIX_H
+#define SERVEROK_MATRIX_H
+
+#define MAXLEN 256
+
+struct matrixargs {
+    int N;
+    int* MT1;
+    int* MT2;
+    int* res;
+    int start;
+    int end;
+} args[50];
+
+#endif //SERVEROK_MATRIX_H
